4811ab75568f8dba1ecfffa8fa6f8d5afa1678c2
Leandro Henrique Luz Leite <leandro.henrique@sonda.com>
2024-08-23 20:05:10 -0300

    Removido microsserviço que não faz parte do projeto

--- ordems-save-ms/src/main/java/com/btg/java/ordems/controllers/dtos/ApiResponseDto.java
+++
@@ -1,8 +0,0 @@
-package com.btg.java.ordems.controllers.dtos;
-
-import java.util.List;
-import java.util.Map;
-
-public record ApiResponseDto<T>(Map<String, Object> summary, List<T> data,
-                                PaginationResponseDto paginationResponseDto) {
-}
