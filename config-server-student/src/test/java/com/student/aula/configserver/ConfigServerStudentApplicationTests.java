7795ce339a3b669a84ff4746064ae4128edc34ed
leandro.henrique <leandroluz201616@gmail.com>
2024-09-25 14:52:20 -0300

    feat: Adicionado config server service

---
+++ config-server-student/src/test/java/com/student/aula/configserver/ConfigServerStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student.aula.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerStudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
