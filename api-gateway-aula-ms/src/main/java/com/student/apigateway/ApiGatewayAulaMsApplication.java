56461c7beaa2e2e0aa8468a812e5a1ce76356e14
leandro.henrique <leandroluz201616@gmail.com>
2024-09-25 14:51:51 -0300

    feat: Adicionado API Gateway service

---
+++ api-gateway-aula-ms/src/main/java/com/student/apigateway/ApiGatewayAulaMsApplication.java
@@ -0,0 +1,14 @@
+package com.student.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ApiGatewayAulaMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayAulaMsApplication.class, args);
+    }
+
+}
