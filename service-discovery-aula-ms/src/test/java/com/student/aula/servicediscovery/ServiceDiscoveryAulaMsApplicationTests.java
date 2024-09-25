9b61903014412b65d4157cdb69f4eeef00bbe58f
leandro.henrique <leandroluz201616@gmail.com>
2024-09-25 14:50:18 -0300

    feat: Adicionado service Discovery

---
+++ service-discovery-aula-ms/src/test/java/com/student/aula/servicediscovery/ServiceDiscoveryAulaMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student.aula.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryAulaMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
